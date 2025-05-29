bb273495301c79ab0e2bc8d063ebafba817673a7
JiinSeok <126224323+JiinSeok@users.noreply.github.com>
2025-05-29 18:26:51 +0900

    fix: set foreign image url

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type {NextConfig} from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
+  eslint: {ignoreDuringBuilds: true},
+  images: {
+    remotePatterns: [
+      {protocol: 'https', hostname: 'cdn.simpleicons.org'},
+      {protocol: 'https', hostname: 'cdn.jsdelivr.net'},
+      {protocol: 'https', hostname: 'github.com'},
+    ],
+    dangerouslyAllowSVG: true,
+  },
 }
 
 export default nextConfig
