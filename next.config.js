cd388a90cec4ace516f8423b4c1f16d0c16ce5bd
JiinSeok <126224323+JiinSeok@users.noreply.github.com>
2025-05-31 20:01:40 +0900

    feat: add amplify config

--- next.config.js
+++ next.config.js
@@ -2,25 +2,25 @@ const path = require('path')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  eslint: { ignoreDuringBuilds: true },
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
-      { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
-      { protocol: 'https', hostname: 'cdn.simpleicons.org' },
-      { protocol: 'https', hostname: 'cdn.jsdelivr.net' },
-      { protocol: 'https', hostname: 'github.com' },
-    ],
-    dangerouslyAllowSVG: true,
-  },
-  // Include content directory in transpilation
-  transpilePackages: ['content'],
-  // Add content directory to webpack resolve modules
-  webpack: (config) => {
-    config.resolve.modules.push(path.resolve('./content'))
-    return config
-  },
+    output: 'standalone',
+    eslint: { ignoreDuringBuilds: true },
+    images: {
+        remotePatterns: [
+            { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+            { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
+            { protocol: 'https', hostname: 'cdn.simpleicons.org' },
+            { protocol: 'https', hostname: 'cdn.jsdelivr.net' },
+            { protocol: 'https', hostname: 'github.com' },
+        ],
+        dangerouslyAllowSVG: true,
+    },
+    // Include content directory in transpilation
+    transpilePackages: ['content'],
+    // Add content directory to webpack resolve modules
+    webpack: (config) => {
+        config.resolve.modules.push(path.resolve('./content'))
+        return config
+    },
 }
 
 module.exports = nextConfig
